dfa88f8dad0b42b389950f5cfe291ccf0fa38fd4
hardikpython <pk646155@gmail.com>
2025-02-07 20:25:47 +0530

    andoroperator

---
+++ andoroperator.py
@@ -0,0 +1,14 @@
+a=10
+b=20
+c=0
+if a and b and c:
+ print("all the numbers have boolean values as true")
+else:
+ print("at least one number has boolean values as false")
+ a=10
+ b=-10
+ c=0
+ if a>0 or b>0:
+  print("either of the number is greater than 0")
+ else:
+  print("no number is greater than 0")
