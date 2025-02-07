308ed674e880a3a3ba0183150fd7f7ca756b0848
hardikpython <pk646155@gmail.com>
2025-02-07 20:40:16 +0530

    notequaloperator

---
+++ notequaloperator.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+a="hardik"
+b="kumar"
+if a!=b:
+ print("a and b are different")
+x=int(input("enter a number"))
+if x%2!=0:
+  print(x, "4
+  is not even number")
+  
+ 
+
+
+
+
